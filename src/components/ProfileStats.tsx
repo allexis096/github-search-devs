d9d9147a459c6e0ce87c33167c3e3611761258d6
allexis096 <allexissoares@gmail.com>
2021-04-21 16:27:36 -0300

    :sparkles: feat: add ProfileStatus component

---
+++ src/components/ProfileStats.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Flex, Icon, Text } from '@chakra-ui/react';
+import { IconType } from 'react-icons/lib';
+
+type ProfileStatsProps = {
+  icon: IconType;
+  title: string;
+};
+
+export function ProfileStats({ icon: as, title }: ProfileStatsProps) {
+  return (
+    <Flex mt="3" mr="2">
+      <Icon as={as} fontSize={20} color="white.200" mr="1" />
+      <Text color="white.200" fontWeight="light" fontSize="smaller">
+        {title}
+      </Text>
+    </Flex>
+  );
+}
